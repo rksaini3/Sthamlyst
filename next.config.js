0ed2b8c8244d61afc60a3a5ad4c600b1bf1a8f9d
Raja Saini <rajasaini22086@gmail.com>
2026-08-30 09:53:54 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const withPWA = require('next-pwa')({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  fallbacks: {
+    document: '/offline.html',
+  },
 })
 
 const nextConfig = {
