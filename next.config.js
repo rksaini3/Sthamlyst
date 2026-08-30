5650782e64be0c17740ea171e744a420a0f8e048
Raja Saini <rajasaini22086@gmail.com>
2026-08-30 10:25:07 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,9 +4,28 @@ const withPWA = require('next-pwa')({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  cleanupOutdatedCaches: true,
   fallbacks: {
     document: '/offline.html',
   },
+  runtimeCaching: [
+    {
+      urlPattern: ({ url }) => url.hostname.includes('supabase.co'),
+      handler: 'NetworkOnly',
+    },
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'pages-cache',
+        networkTimeoutSeconds: 3,
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60,
+        },
+      },
+    },
+  ],
 })
 
 const nextConfig = {
