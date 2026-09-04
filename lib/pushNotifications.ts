dcccec6e03ef4ec1c2f8120fd408ec3614ae127f
Raja Saini <rajasaini22086@gmail.com>
2026-09-04 08:15:28 +0530

    Update pushNotifications.ts

--- lib/pushNotifications.ts
+++ lib/pushNotifications.ts
@@ -48,7 +48,7 @@ export async function registerPushNotifications() {
 
       subscription = await registration.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(vapidKey),
+        applicationServerKey: urlBase64ToUint8Array(vapidKey) as BufferSource,
       });
     }
 
