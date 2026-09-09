6299fedb6024283c5146c38f604d603724f2255f
Raja Saini <rajasaini22086@gmail.com>
2026-09-09 20:10:42 +0530

    Create components/ServiceWorkerRegister.tsx

---
+++ components/ServiceWorkerRegister.tsx
@@ -0,0 +1,21 @@
+'use client'
+import { useEffect } from 'react'
+
+export default function ServiceWorkerRegister() {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').then((reg) => {
+        reg.addEventListener('updatefound', () => {
+          const newWorker = reg.installing
+          newWorker?.addEventListener('statechange', () => {
+            if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
+              newWorker.postMessage({ type: 'SKIP_WAITING' })
+            }
+          })
+        })
+      }).catch((err) => console.error('SW registration failed:', err))
+    }
+  }, [])
+
+  return null
+}
