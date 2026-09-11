edf61d34fc007efd14c52262ef36f8641bc36bb5
Raja Saini <rajasaini22086@gmail.com>
2026-09-11 08:31:01 +0530

    Delete page.tsx

--- app/learn/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation'
-
-// The Learn feed is merged into the Instagram-style home feed (app/page.tsx).
-export default function LearnRedirect() {
-  redirect('/')
-}
