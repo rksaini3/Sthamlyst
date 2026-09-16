9f8659da2b2aeb7fdb3f376522c084897d989eb2
Raja Saini <rajasaini22086@gmail.com>
2026-09-16 19:22:21 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from 'next'
 
-const BASE_URL = 'https://sthamly.com'
+const BASE_URL = 'https://www.sthamly.com'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const lastModified = new Date()
