b5191d234cba10c4a423a76bee68f69fd2384436
Raja Saini <rajasaini22086@gmail.com>
2026-09-16 19:23:04 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from 'next'
 
-const BASE_URL = 'https://sthamly.com'
+const BASE_URL = 'https://www.sthamly.com'
 
 export default function robots(): MetadataRoute.Robots {
   return {
